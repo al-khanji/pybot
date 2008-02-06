e10ee458dc6615c1fe87592e4d196c03f91b43ef
slougi@cn0005 <slougi@cn0005>
2008-02-06 23:17:21 +0200

    Add newline

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         irc.do_connections(connections)
 
     for connection in connections:
-        connection.quit(irc.quit)
+        connection.quit()
 
 if __name__ == "__main__":
     main()
