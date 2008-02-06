3193615ef0e084270c2ae69a23b671b1b01d573a
slougi@cn0005 <slougi@cn0005>
2008-02-06 23:16:51 +0200

    Add comment

--- config.py
+++ config.py
@@ -4,16 +4,19 @@ defaults = {
     "nick": "slougibot",
     "realname": "minä olen botti",
     "port": 6667,
-    "mode": 0,
-    "ssl": False
+    "mode": 0, # 8 = +i, 4 = +w, 12 = +wi
+    "ssl": False,
+    "channels": [],
+    "quit_message": "My master bade me \"Quit thy lurking!\""
 }
 
 networks = [
-    #{ "server": "irc.cs.tut.fi", "channels": ["#toimisto.org"] }
-    { "server": "irc.virtues.fi",
-      "channels": ["#clarinet"],
-      "port": 994,
-      "ssl": True }
+#    { "server": "irc.cs.tut.fi", "channels": ["#banaaniloota"] },
+#    { "server": "irc.virtues.fi",
+#      "channels": ["#clarinet"],
+#      "port": 994,
+#      "ssl": True }
+    { "server": "efnet.demon.co.uk", "channels": ["#perjantai"] }
 ]
 
 # don't touch stuff below this line please
