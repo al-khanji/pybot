0c97fe72d669750b2d165865404eeda5dd098842
slougi@CN0005 <slougi@CN0005>
2008-02-10 23:03:53 +0200

    Better status reporting.

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ def main():
             connection = irc.Connection(network)
             connection.connect()
             connections.append(connection)
-        except irc.ConnectionError:
-            print "Error connecting to %s, removing from connection list" \
-                   % network["server"]
+        except irc.ConnectionError, e:
+            print "Error connecting to %s ->" % network["server"], e
+            print "Removing from connections."
 
     while irc.quit is False and len(connections) != 0:
         irc.do_connections(connections)
+
+    print
 
     if irc.quit is not False:
         print "Quit requested, closing remaining connections"
         for connection in connections:
             connection.quit()
-
     if len(connections) == 0:
-        print "No connections left, all done here."
+        print "No connections left."
+
+    print
+    print "All done."
 
 if __name__ == "__main__":
     main()
